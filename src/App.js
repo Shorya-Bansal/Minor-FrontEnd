a8b9467bdb98d64d6b2feb8411524e07bb261ab9
Kartikey Vaish <kartikeyvaish99@gmail.com>
2021-06-11 19:55:47 +0530

    Project Cleaned

--- src/App.js
+++ src/App.js
@@ -1,58 +1,46 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
+import { Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
 
-import Login from "./components/Login";
-/* import AdminLogin from "./components/AdminLogin";*/
-import Register from './components/Register';
+import AuthContext from "./auth/context";
 import Home from "./components/Home";
 import Help from "./components/Help";
-
-import AuthContext from "./auth/context"
-
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
+  const [User, SetUser] = useState(null);
+  const [IsReady, SetIsReady] = useState(false);
 
-    const [User, SetUser] = useState(null);
-    const [IsReady, SetIsReady] = useState(false);
+  useEffect(() => {
+    RestoreToken();
+  }, []);
 
+  const RestoreToken = async () => {
+    const result = localStorage.getItem("auth-token");
 
-    useEffect(() => {
-        RestoreToken();
-    }, []);
-
-    const RestoreToken = async () => {
-        const result = localStorage.getItem("auth-token");
-
-        if (result) {
-            const user = jwtDecode(result);
-            SetUser(user);
-        }
-        SetIsReady(true);
-    };
-    if (!IsReady) {
-        return (
-            <div></div>
-        );
+    if (result) {
+      const user = jwtDecode(result);
+      SetUser(user);
     }
-    return (
-        <AuthContext.Provider
-            value={{ User, SetUser }}
-        >
-            <div>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/" exact component={Login} ></Route>
-                        {/* <Route path="/admin" exact component={AdminLogin}></Route> */}
-                        <Route path="/admin/register" exact component={Register}></Route>
-                        <Route path="/home" exact component={Home}></Route>
-                        <Route path="/help" exact component={Help}></Route>
+    SetIsReady(true);
+  };
 
-                    </Switch>
-                </BrowserRouter>
-            </div>
-        </AuthContext.Provider>
-    )
+  if (!IsReady) {
+    return <div></div>;
+  }
+
+  return (
+    <AuthContext.Provider value={{ User, SetUser }}>
+      <Switch>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/register" exact component={Register}></Route>
+        <Route path="/home" exact component={Home}></Route>
+        <Route path="/help" exact component={Help}></Route>
+        <Route path="/" exact component={Home}></Route>
+      </Switch>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
-import jwtDecode from "jwt-decode";
+import React, { useContext } from "react";
 
-import AuthContext from './../auth/context';
+import AuthContext from "./../auth/context";
 import Navbar from "./NavBar";
 
-import "../css/home.css"
+import "../css/home.css";
 
 function Home() {
-    const [payload, setPayload] = useState("");
-    const authContext = useContext(AuthContext)
-    return (
-        <div>
-            <Navbar user={authContext.User}></Navbar>
-            <div className="home-main" style={{ width: window.innerWidth < 769 ? "100%" : "82%" }} >
-                <h1>Hello Shorya</h1>
-            </div>
-        </div>
-    );
+  const authContext = useContext(AuthContext);
+
+  return (
+    <div>
+      <Navbar user={authContext.User}></Navbar>
+      <div
+        className="home-main"
+        style={{ width: window.innerWidth < 769 ? "100%" : "82%" }}
+      >
+        <h1>Hello Shorya</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
